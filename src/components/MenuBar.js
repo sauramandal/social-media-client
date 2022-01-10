09086e50e4eb5a504826af8d242680be0dea8cbe
Saura Mandal <saura.mandal1@gmail.com>
2022-01-10 15:13:34 +0530

    Persist token in localStorage

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,23 +1,34 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
+import { AuthContext } from '../context/auth'
 
 const MenuBar = () => {
-    const pathname = window.location.pathname;
+    const { user, logout } = useContext(AuthContext)
+    const pathname = window.location.pathname
     const path = pathname === '/' ? 'home' : pathname.substring(1)
     const [activeItem, setActiveItem] = useState(path)
-    
+
     const handleItemClick = (e, { name }) => setActiveItem(name)
 
     return (
         <div>
-            <Menu pointing secondary size="massive" color="teal">
-                <Menu.Item name="home" active={activeItem === 'home'} onClick={handleItemClick} as={Link} to="/" />
-                <Menu.Menu position="right">
-                    <Menu.Item name="login" active={activeItem === 'login'} onClick={handleItemClick} as={Link} to="/login" />
-                    <Menu.Item name="register" active={activeItem === 'register'} onClick={handleItemClick} as={Link} to="/register" />
-                </Menu.Menu>
-            </Menu>
+            {!user ? (
+                <Menu pointing secondary size="massive" color="teal">
+                    <Menu.Item name="home" active={activeItem === 'home'} onClick={handleItemClick} as={Link} to="/" />
+                    <Menu.Menu position="right">
+                        <Menu.Item name="login" active={activeItem === 'login'} onClick={handleItemClick} as={Link} to="/login" />
+                        <Menu.Item name="register" active={activeItem === 'register'} onClick={handleItemClick} as={Link} to="/register" />
+                    </Menu.Menu>
+                </Menu>
+            ) : (
+                <Menu pointing secondary size="massive" color="teal">
+                    <Menu.Item name={user.username} active={activeItem === 'home'} as={Link} to="/" />
+                    <Menu.Menu position="right">
+                        <Menu.Item name="logout" onClick={logout} />
+                    </Menu.Menu>
+                </Menu>
+            )}
         </div>
     )
 }
