fe30546ea9e1421ca43f99330d758cca7898dbf4
Saura Mandal <saura.mandal1@gmail.com>
2022-01-12 12:19:16 +0530

    Add transition group for new post addition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useContext } from 'react'
 import { useQuery } from '@apollo/client'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Transition } from 'semantic-ui-react'
 import PostCard from '../components/PostCard'
 import PostForm from '../components/PostForm'
 import { AuthContext } from '../context/auth'
@@ -24,12 +24,14 @@ const Home = () => {
                     {loading ? (
                         <h1>Loading posts...</h1>
                     ) : (
-                        data.getPosts &&
-                        data.getPosts.map((post) => (
-                            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                                <PostCard post={post} />
-                            </Grid.Column>
-                        ))
+                        <Transition.Group>
+                            {data.getPosts &&
+                                data.getPosts.map((post) => (
+                                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                        <PostCard post={post} />
+                                    </Grid.Column>
+                                ))}
+                        </Transition.Group>
                     )}
                 </Grid.Row>
             </Grid>
